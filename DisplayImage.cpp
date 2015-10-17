39e08a44ce624a58e5b20aa69c9c5785a9f83e7b
Jacob White <jacob.white@byu.net>
2015-10-16 20:56:08 -0600

    Calculate distance from HSV values. Vary lambda.

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -42,20 +42,22 @@ int maxVal3(int val1, int val2, int val3)
 	return maxVal(maxVal(val1, val2), val3);
 }
 
-int getDiffRGB(cv::Vec<unsigned char, 3> val1, cv::Vec<unsigned char, 3> val2)
+int getDiffHSV(cv::Vec<unsigned char, 3> val1, cv::Vec<unsigned char, 3> val2)
 {
-	return maxVal3(abs(val1[0] - val2[0]), abs(val1[1] - val2[1]), abs(val1[2] - val2[2]));
+	//For HSV, Hue range is [0,179], Saturation range is [0,255] and Value range is [0,255]
+	return abs(val1[0] - val2[0]) + abs(val1[1] - val2[1])/4 + abs(val1[2] - val2[2])/4;
 }
 
 int getCost(cv::Vec<unsigned char, 3> val1, cv::Vec<unsigned char, 3> val2)
 {
-	return 100 / (getDiffRGB(val1, val2) + 1);
+	return 254 / (getDiffHSV(val1, val2) + 1);
 }
 
-int addEdge(GraphType* g, Mat image, int x, int y, int dx, int dy)
+int addEdge(GraphType* g, Mat image, int x, int y, int dx, int dy, int lambda)
 {
 	int cost = getCost(image.at<Vec3b>(y, x), image.at<Vec3b>(y + dy, x + dx));
-	g->add_edge(getNodeId(image, x, y), getNodeId(image, x + dx, y + dy), cost, cost);
+	int modifiedCost = cost * (100 + lambda) / 100;
+	g->add_edge(getNodeId(image, x, y), getNodeId(image, x + dx, y + dy), modifiedCost, modifiedCost);
 	return cost;
 }
 
@@ -73,7 +75,7 @@ DistFB findDistFB(cv::Vec<unsigned char, 3> val, SeedType seeds)
 	for(int i = 0; i < seeds.size(); i++)
 	{
 		Seed s = seeds.at(i);
-		int dist = getDiffRGB(val, s.color);
+		int dist = getDiffHSV(val, s.color);
 		if(s.foreground)
 		{
 			if(dist < minDistForeground)
@@ -88,7 +90,8 @@ DistFB findDistFB(cv::Vec<unsigned char, 3> val, SeedType seeds)
 	return (DistFB){minDistForeground, minDistBackground};
 }
 
-void displayResults(Mat image, SeedType seeds)
+void displayResults(Mat image, Mat hsv, SeedType seeds)
+void displayResults(Mat image, Mat hsv, SeedType seeds, int lambda)
 {
 	GraphType* g = new GraphType(image.rows * image.cols, 2 * image.rows * image.cols - image.rows - image.cols);
 
@@ -99,10 +102,10 @@ void displayResults(Mat image, SeedType seeds)
 		for(int x = 0; x < image.cols; x++)
 		{
 			g->add_node();
-			DistFB d = findDistFB(image.at<Vec3b>(y,x), seeds);
-			int sourceWeight = 10*d.db/(d.df + d.db + 1);
-			int sinkWeight = 10*d.df/(d.df + d.db + 1);
-			g->add_tweights(getNodeId(image, x, y), sourceWeight, sinkWeight);
+			DistFB d = findDistFB(hsv.at<Vec3b>(y,x), seeds);
+			int sourceWeight = 100*d.db/(d.df + d.db + 1);
+			int sinkWeight = 100*d.df/(d.df + d.db + 1);
+			g->add_tweights(getNodeId(image, x, y), sourceWeight * (100 - lambda) / 100, sinkWeight * (100 - lambda) / 100);
 			priors1.at<Vec3b>(y,x)[0] = sourceWeight;
 			priors1.at<Vec3b>(y,x)[1] = sourceWeight;
 			priors1.at<Vec3b>(y,x)[2] = sourceWeight;
@@ -110,8 +113,11 @@ void displayResults(Mat image, SeedType seeds)
 			priors2.at<Vec3b>(y,x)[1] = sinkWeight;
 			priors2.at<Vec3b>(y,x)[2] = sinkWeight;
 		}
-	imwrite("WeightFg.jpg", priors1);
-	imwrite("WeightBg.jpg", priors2);
+	imwrite("weightFg.jpg", priors1);
+	imwrite("weightBg.jpg", priors2);
+	imshow("weightFg", priors1);
+	imshow("weightBg", priors2);
+
 
 	// Add edges with corresponding cost
 	Mat cost = Mat::zeros(image.size(), image.type());
@@ -120,15 +126,17 @@ void displayResults(Mat image, SeedType seeds)
 		{
 			int c1, c2 = 0;
 			if(x < image.cols - 1)
-				c1 = addEdge(g, image, x, y, 1, 0);
+				c1 = addEdge(g, hsv, x, y, 1, 0, lambda);
 			if(y < image.rows - 1)
-				c2 = addEdge(g, image, x, y, 0, 1);
+				c2 = addEdge(g, hsv, x, y, 0, 1, lambda);
 			int c = (c1 + c2) / 2;
 			cost.at<Vec3b>(y,x)[0] = c;
 			cost.at<Vec3b>(y,x)[1] = c;
 			cost.at<Vec3b>(y,x)[2] = c;
 		}
 	imwrite("cost.jpg", cost);
+	imshow("cost", cost);
+
 
 	// Add user specified seeds
 	for(int i = 0; i < seeds.size(); i++)
@@ -148,21 +156,15 @@ void displayResults(Mat image, SeedType seeds)
 		{
 			if(g->what_segment(getNodeId(image, x, y)) == GraphType::SOURCE)
 			{
-//				result.at<Vec3b>(y,x)[0] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[0]);
-//				result.at<Vec3b>(y,x)[1] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[1]);
-//				result.at<Vec3b>(y,x)[2] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[2]);
-				result.at<Vec3b>(y,x)[0] = 255;
-				result.at<Vec3b>(y,x)[1] = 255;
-				result.at<Vec3b>(y,x)[2] = 255;
+				result.at<Vec3b>(y,x)[0] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[0]);
+				result.at<Vec3b>(y,x)[1] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[1]);
+				result.at<Vec3b>(y,x)[2] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[2]);
 			}
 			else
 			{
-				result.at<Vec3b>(y,x)[0] = 0;
-				result.at<Vec3b>(y,x)[1] = 0;
-				result.at<Vec3b>(y,x)[2] = 0;
-//				result.at<Vec3b>(y,x)[0] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[0]) / 3;
-//				result.at<Vec3b>(y,x)[1] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[1]) / 3;
-//				result.at<Vec3b>(y,x)[2] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[2]) / 3;
+				result.at<Vec3b>(y,x)[0] = 255;
+				result.at<Vec3b>(y,x)[1] = 255;
+				result.at<Vec3b>(y,x)[2] = 255;
 			}
 		}
 
@@ -174,7 +176,7 @@ void displayResults(Mat image, SeedType seeds)
 
 SeedType seeds;
 
-void addSeed(GraphType* g, Mat image, int x, int y, bool foreground, cv::Vec<unsigned char, 3> color)
+void addSeed(int x, int y, bool foreground, cv::Vec<unsigned char, 3> color)
 {
 	seeds.push_back((Seed){x, y, foreground, color});
 }
@@ -187,7 +189,10 @@ void addSeed(GraphType* g, Mat image, int x, int y, bool foreground, cv::Vec<uns
 int mouseDown;
 Mat image2;
 Mat originalImage;
+Mat hsvImage;
 GraphType* graph;
+
+int lambda = 0;
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
@@ -202,9 +207,9 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 	if (mouseDown)
 	{
 		if(mouseDown == 1)
-			addSeed(graph, originalImage, x, y, true, image2.at<Vec3b>(y,x));
+			addSeed(x, y, true, hsvImage.at<Vec3b>(y,x));
 		else
-			addSeed(graph, originalImage, x, y, false, image2.at<Vec3b>(y,x));
+			addSeed(x, y, false, hsvImage.at<Vec3b>(y,x));
 
 		Scalar color = (mouseDown == 1) ? Scalar(255, 0, 0) : Scalar(0, 0, 255);
 		circle(image2, Point(x, y), 3, color, CV_FILLED);
@@ -221,6 +226,7 @@ int main(int argc, char** argv )
 	}
 
 	originalImage = imread( argv[1], 1 );
+	cvtColor(originalImage, hsvImage, COLOR_BGR2HSV);
 	image2 = originalImage.clone();
 
 	if ( !image2.data )
@@ -239,7 +245,66 @@ int main(int argc, char** argv )
 		char c = waitKey(0);
 		if(c == ESC)
 			break;
-		displayResults(originalImage, seeds);
+		if(c == 'r')
+		{
+			image2 = originalImage.clone();
+			seeds = SeedType();
+			imshow("Display Image", image2);
+		}
+		else if(c == 'h')
+		{
+			Mat bgr;
+			Mat modified = hsvImage.clone();
+			for( int y = 0; y < modified.rows; y++ )
+				for( int x = 0; x < modified.cols; x++ )
+				{
+					modified.at<Vec3b>(y,x)[0] = saturate_cast<uchar>(modified.at<Vec3b>(y,x)[0]);
+					modified.at<Vec3b>(y,x)[1] = 128;
+					modified.at<Vec3b>(y,x)[2] = 128;
+				}
+			cvtColor(modified, bgr, COLOR_HSV2BGR);
+			imshow("Hue", bgr);
+		}
+		else if(c == 's')
+		{
+			Mat modified = originalImage.clone();
+			for( int y = 0; y < modified.rows; y++ )
+				for( int x = 0; x < modified.cols; x++ )
+				{
+					int val = saturate_cast<uchar>(modified.at<Vec3b>(y,x)[1]);
+					modified.at<Vec3b>(y,x)[0] = val;
+					modified.at<Vec3b>(y,x)[1] = val;
+					modified.at<Vec3b>(y,x)[2] = val;
+				}
+			imshow("Saturation", modified);
+		}
+		else if(c == 'v')
+		{
+			Mat modified = originalImage.clone();
+			for( int y = 0; y < modified.rows; y++ )
+				for( int x = 0; x < modified.cols; x++ )
+				{
+					int val = saturate_cast<uchar>(modified.at<Vec3b>(y,x)[2]);
+					modified.at<Vec3b>(y,x)[0] = val;
+					modified.at<Vec3b>(y,x)[1] = val;
+					modified.at<Vec3b>(y,x)[2] = val;
+				}
+			imshow("Value", modified);
+		}
+		else if(c == 'l')
+		{
+			printf("Type in the new lambda:\n");
+			cin >> lambda;
+			printf("Lambda is now %d\n", lambda);
+			imwrite("seeds.jpg", image2);
+			displayResults(originalImage, hsvImage, seeds, lambda);
+		}
+		else
+		{
+//			printf("character: now %d\n", (int)c);
+			imwrite("seeds.jpg", image2);
+			displayResults(originalImage, hsvImage, seeds, lambda);
+		}
 	}
 	delete graph;
 	return 0;
