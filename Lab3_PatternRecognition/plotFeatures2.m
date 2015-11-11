98aa7d15e99a3c4dde619f72ef08d2c56679ce0c
Jacob White <jacob.white@byu.net>
2015-11-10 21:01:18 -0700

    Create single-line plots and legend. Use variable to switch between data sets.

--- Lab3_PatternRecognition/plotFeatures2.m
+++ Lab3_PatternRecognition/plotFeatures2.m
@@ -1,6 +1,6 @@
 colorArray = ...
 [0, 0, 0; ...
-0, 64, 128;  ...
+0, 74, 148;  ...
 0, 0, 255;   ...
 0, 128, 255; ...
 0, 255, 255; ...
@@ -11,31 +11,50 @@ colorArray = ...
 200, 200, 200];
 colorArray = [colorArray(:, 3) colorArray(:, 2) colorArray(:, 1)] / 255;
 
-%all = [shapes; testshapes; train1; train2; match1; match2];
-%all = [shapes; testshapes];
-all = [train1; train2; match1; match2];
+choice = 1;
+if choice == 0
+	%all = [shapes; testshapes; train1; train2; match1; match2];
+	all = [shapes; testshapes];
+else
+	all = [train1; train2; match1; match2];
+end
 f1 = 'eccentricity';
-f2 = 'rectDensity'; %***
-f3 = 'scaledPerimeter';
-f4 = 'hullDensity'; %*
+f2 = 'rectangular bounding box density'; %***
+f3 = 'normalized perimeter';
+f4 = 'convex hull density'; %*
 f5 = 'density';
-xlabel([f2 ' + ' f5]);
-ylabel(f3);
-%zlabel(f3);
+
+if choice == 0
+	xlabel(f3);
+	ylabel(f5);
+else
+	xlabel([f2 ' + ' f5]);
+	ylabel(f3);
+end
+
 hold on;
 sums   = zeros([10 2]);
 counts = zeros([10 1]);
 
-z = 1
-
-for i = 1:length(all);
-	shapeNumber = all(i, 6);
-	plot(all(i, 2) + all(i, 5), all(i, 3), '*', 'Color', colorArray(shapeNumber + 1, :));
-	sums(shapeNumber + 1, :) = sums(shapeNumber + 1, :) + [all(i, 2) + all(i, 5), all(i, 3)];
-	counts(shapeNumber + 1) = counts(shapeNumber + 1) + 1;
+usedFeatures = zeros(length(all), 2);
+for shapeNumber = 0:9
+	xc = [];
+	for i = 1:length(all);
+		if shapeNumber == all(i, 6)
+			if choice == 0
+				usedFeatures(i, :) = [all(i, 3), all(i, 5)];
+			else
+				usedFeatures(i, :) = [all(i, 2) + all(i, 5), all(i, 3)];
+			end
+			xc = [xc; usedFeatures(i, :)];
+			sums(shapeNumber + 1, :) = sums(shapeNumber + 1, :) + usedFeatures(i, :);
+			counts(shapeNumber + 1) = counts(shapeNumber + 1) + 1;
+		end
+	end
+	if ~isempty(xc)
+		plot(xc(:, 1), xc(:, 2), '*', 'Color', colorArray(shapeNumber + 1, :));
+	end
 end
-
-
 
 if counts(1) >= 1, u1 = sums(1, :) / counts(1); else u1 = [1 1] * 1000; end
 if counts(2) >= 1, u2 = sums(2, :) / counts(2); else u2 = [1 1] * 1000; end
@@ -47,72 +66,40 @@ if counts(7) >= 1, u7 = sums(7, :) / counts(7); else u7 = [1 1] * 1000; end
 if counts(8) >= 1, u8 = sums(8, :) / counts(8); else u8 = [1 1] * 1000; end
 if counts(9) >= 1, u9 = sums(9, :) / counts(9); else u9 = [1 1] * 1000; end
 if counts(10) >= 1, u10 = sums(10, :) / counts(10); else u10 = [1 1] * 1000; end
+u = [u1; u2; u3; u4; u5; u6; u7; u8; u9; u10];
 
-if sum(u1) < 1000, plot(u1(1), u1(2), 'o', 'Color', colorArray(1, :)); end
-if sum(u2) < 1000, plot(u2(1), u2(2), 'o', 'Color', colorArray(2, :)); end
-if sum(u3) < 1000, plot(u3(1), u3(2), 'o', 'Color', colorArray(3, :)); end
-if sum(u4) < 1000, plot(u4(1), u4(2), 'o', 'Color', colorArray(4, :)); end
-if sum(u5) < 1000, plot(u5(1), u5(2), 'o', 'Color', colorArray(5, :)); end
-if sum(u6) < 1000, plot(u6(1), u6(2), 'o', 'Color', colorArray(6, :)); end
-if sum(u7) < 1000, plot(u7(1), u7(2), 'o', 'Color', colorArray(7, :)); end
-if sum(u8) < 1000, plot(u8(1), u8(2), 'o', 'Color', colorArray(8, :)); end
-if sum(u9) < 1000, plot(u9(1), u9(2), 'o', 'Color', colorArray(9, :)); end
-if sum(u10) < 1000, plot(u10(1), u10(2), 'o', 'Color', colorArray(10, :)); end
+xc = [];
 
+W = 1./(max(usedFeatures) - min(usedFeatures));
 
-xc1 = [];
-xc2 = [];
-xc3 = [];
-xc4 = [];
-xc5 = [];
-xc6 = [];
-xc7 = [];
-xc8 = [];
-xc9 = [];
-xc10 = [];
-
-W = [15 1];
-
-% for each point, calculate P(x | c_i)
+if choice == 0
+	ylim([.1 1.1])
+end
 ax = axis;
-tickX = (ax(2) - ax(1)) / 100;
-tickY = (ax(4) - ax(3)) / 100;
-for x_1 = ax(1):tickX:ax(2)
-	for x_2 = 3:tickY:12
-		x = [x_1 x_2];
-		d1 = norm((x - u1) .* W);
-		d2 = norm((x - u2) .* W);
-		d3 = norm((x - u3) .* W);
-		d4 = norm((x - u4) .* W);
-		d5 = norm((x - u5) .* W);
-		d6 = norm((x - u6) .* W);
-		d7 = norm((x - u7) .* W);
-		d8 = norm((x - u8) .* W);
-		d9 = norm((x - u9) .* W);
-		d10 = norm((x - u10) .* W);
-		
-		dMin = min([d1 d2 d3 d4 d5 d6 d7 d8 d9 d10]);
-		if     dMin == d1, xc1 = [xc1; x]; %This is about 10 times faster!
-		elseif dMin == d2, xc2 = [xc2; x];
-		elseif dMin == d3, xc3 = [xc3; x];
-		elseif dMin == d4, xc4 = [xc4; x];
-		elseif dMin == d5, xc5 = [xc5; x];
-		elseif dMin == d6, xc6 = [xc6; x];
-		elseif dMin == d7, xc7 = [xc7; x];
-		elseif dMin == d8, xc8 = [xc8; x];
-		elseif dMin == d9, xc9 = [xc9; x];
-		elseif dMin == d10, xc10 = [xc10; x];
+divisions = 500;
+tickX = (ax(2) - ax(1)) / divisions;
+tickY = (ax(4) - ax(3)) / divisions;
+d = zeros([1 10]);
+s = zeros([divisions divisions]);
+for x_2 = 1:divisions
+	for x_1 = 1:divisions
+		x = [ax(1)+tickX*x_1 ax(3)+tickY*x_2];
+		for i = 1:10
+			d(i) = norm((x - u(i, :)) .* W);
+		end
+		[~, region] = min(d);
+		s(x_1, x_2) = region;
+		if (x_1 > 1 && region ~= s(x_1 - 1, x_2)) || ...
+			(x_2 > 1 && region ~= s(x_1, x_2 - 1))
+			xc = [xc; x];
 		end
 	end
 end
 
-if ~isempty(xc1), plot(xc1(:, 1), xc1(:, 2), '.', 'Color', colorArray(1, :)); end
-if ~isempty(xc2), plot(xc2(:, 1), xc2(:, 2), '.', 'Color', colorArray(2, :)); end
-if ~isempty(xc3), plot(xc3(:, 1), xc3(:, 2), '.', 'Color', colorArray(3, :)); end
-if ~isempty(xc4), plot(xc4(:, 1), xc4(:, 2), '.', 'Color', colorArray(4, :)); end
-if ~isempty(xc5), plot(xc5(:, 1), xc5(:, 2), '.', 'Color', colorArray(5, :)); end
-if ~isempty(xc6), plot(xc6(:, 1), xc6(:, 2), '.', 'Color', colorArray(6, :)); end
-if ~isempty(xc7), plot(xc7(:, 1), xc7(:, 2), '.', 'Color', colorArray(7, :)); end
-if ~isempty(xc8), plot(xc8(:, 1), xc8(:, 2), '.', 'Color', colorArray(8, :)); end
-if ~isempty(xc9), plot(xc9(:, 1), xc9(:, 2), '.', 'Color', colorArray(9, :)); end
-if ~isempty(xc10), plot(xc10(:, 1), xc10(:, 2), '.', 'Color', colorArray(10, :)); end
+plot(xc(:, 1), xc(:, 2), '.k');
+
+if choice == 0
+	legend('rectangle', 'triangle', 'circle', 'millstone', 'oval');
+else
+	legend('millstone', 'rounded cross', 'oval', 'E', 'F');
+end
