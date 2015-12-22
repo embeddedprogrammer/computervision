12c10fb6a6c5250212d84306b9f70794bff3334b
Jacob White <jacob.white@byu.net>
2015-12-22 12:38:33 -0700

    Prototype subdivide

--- Lab2_MinCut/Matlab/subdivide.m
+++ Lab2_MinCut/Matlab/subdivide.m
@@ -1,21 +1,25 @@
-% function subdivide(img)
-% 	toInt24 = @(r, g, b) int32(r*256*256 + g*256 + b);
-% 	toRGB = @(v) uint8([mod(floor(v/256/256), 256) mod(floor(v / 256), 256) mod(v, 256)]);
-% 	img(:, :, 1), img(:, :, 1), img(:, :, 1)
-% end
-function toImg24(img)
+function subdivide(img)
+	tic
 	img24 = toInt24(img);
+	allClrs = reshape(img24, [size(img24, 1) * size(img24, 2), 1]);
+	clrs = unique(allClrs, 'rows');
+	size(allClrs)
+	size(clrs)
+	for i = 1:length(clrs)
+		allClrs(allClrs == clrs(i)) = randi(256^3) - 1;
+	end
+	img24 = reshape(allClrs, [size(img24, 1), size(img24, 2)]);
 	imgReproduced = toRGB(img24);
-	size(imgReproduced)
 	imshow(imgReproduced);
 	set(gcf, 'WindowButtonDownFcn', @mouseCb);
-	ax = get(gcf, 'children');	
+	ax = get(gcf, 'children');
 	function mouseCb(~, callbackdata)
 		pos = get(ax(1),'CurrentPoint');
 		pos = int32(pos([1 3]));
 		img(pos(2), pos(1), :)
 		imgReproduced(pos(2), pos(1), :)
-	end	
+	end
+	toc
 end
 function v = toInt24(img)
 	v = int32(img(:,:,1))*256*256 + int32(img(:,:,2))*256 + int32(img(:,:,3));
