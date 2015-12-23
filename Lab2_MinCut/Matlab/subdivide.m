e7783c4d972c0ad836e6e825d64026fdd6990dca
Jacob White <jacob.white@byu.net>
2015-12-23 11:39:28 -0700

    More efficient indexer using sorted arrays. For 20K unique pixels from soccer.png, only takes 1.8s instead of 12.5s.

--- Lab2_MinCut/Matlab/subdivide.m
+++ Lab2_MinCut/Matlab/subdivide.m
@@ -1,21 +1,34 @@
 function subdivide(name)
 	img = imread(name);
 	tic
-	n = 32; %Any more seems to result in loss of color.
+	n = 16; %Any more seems to result in loss of color.
 	img = round(img / n) * n;
-	imshow(img);
-	figure;
+	%figure(1);
+	%imshow(img);
 	%return;
 	img24 = toInt24(img);
 	allClrs = reshape(img24, [size(img24, 1) * size(img24, 2), 1]);
-	clrs = unique(allClrs, 'rows');
-	size(allClrs)
-	size(clrs)
-	for i = 1:length(clrs)
-		allClrs(allClrs == clrs(i)) = randi(256^3) - 1;
+	
+% 	clrs = sortrows(allClrs, 1);
+% 
+% 	size(allClrs)
+% 	size(clrs)
+% 	for i = 1:length(clrs)
+% 		allClrs(allClrs == clrs(i)) = randi(256^3) - 1;
+% 	end
+
+	props = sortCount(allClrs);
+	%length(props)
+	%toc
+	%return;
+	for i = 1:length(props)
+		allClrs(props(i).pixelList) = randi(256^3) - 1;
 	end
+
+	
 	img24 = reshape(allClrs, [size(img24, 1), size(img24, 2)]);
 	imgReproduced = toRGB(img24);
+	figure(2);
 	imshow(imgReproduced);
 	set(gcf, 'WindowButtonDownFcn', @mouseCb);
 	ax = get(gcf, 'children');
